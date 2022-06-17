ff58fab656c4e4aeae3a5257de786dbde30d6627
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-17 23:58:55 +0530

    Added : with registration form , login page successfully completed

--- src/main/java/net/javaguides/springboot/service/UserServiceImpl.java
+++ src/main/java/net/javaguides/springboot/service/UserServiceImpl.java
@@ -19,39 +19,43 @@ import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.web.dto.UserRegistrationDto;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
-	private UserRepository userRepository;
-	
-	//@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
-	
-	public UserServiceImpl(@Lazy UserRepository userRepository) {
+	UserRepository userRepository;
+
+	BCryptPasswordEncoder passwordEncoder;
+
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder ) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public User save(UserRegistrationDto registrationDto) {
-		User user = new User(registrationDto.getFirstName(), 
-				registrationDto.getLastName(), registrationDto.getEmail(),
-				passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
-		
+		User user = null;
+		try {
+			user = new User(registrationDto.getFirstName(), registrationDto.getLastName(), registrationDto.getEmail(),
+         	passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return userRepository.save(user);
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	
+
 		User user = userRepository.findByEmail(username);
-		if(user == null) {
+		if (user == null) {
 			throw new UsernameNotFoundException("Invalid username or password.");
 		}
-		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+				mapRolesToAuthorities(user.getRoles()));
 	}
-	
-	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
 		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
 	}
-	
+
 }
